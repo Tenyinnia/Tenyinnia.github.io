0a987a9d9f2ffec9f6dbf2dbbd618beacd8bfb22
Enyinnia Clifford \tochi <enyinniatochi@gmail.com>
2025-03-27 15:15:59 +0100

    Debug is now False

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w-6!0llo73mxr_m)!zxs_z1n+s!zn#v-u7lo69*#xpfzg6qug8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
